b662878b962bfa483bed1c485d86440cad14f34a
Ainmox <ainmo-x@proton.me>
2023-08-21 03:21:41 -0600

    feat: add semi-fungible friend shares wrapper

---
+++ src/FriendSharesV1.sol
@@ -0,0 +1,62 @@
+pragma solidity >=0.8.2 <0.9.0;
+
+interface FriendSharesV1 {
+    /// @notice Returns the number of `subject` shares that `holder` holds
+    ///
+    /// @param subject The subject of the shares
+    /// @param holder  The holder of the shares
+    ///
+    /// @return shares The number of `subject` shares that `holder` holds
+    function sharesBalance(address subject, address holder) external view returns (uint256 shares);
+
+    /// @notice Returns the number of total number of shares that exist for `subject`
+    ///
+    /// @param subject The subject of the shares
+    ///
+    /// @return totalShares The total number of shares that exist for `subject`
+    function sharesSupply(address subject) external view returns (uint256 totalSupply);
+
+    /// @notice Returns the amount of ether that purchasing `shares` shares of `subject` costs without accounting for fees
+    ///
+    /// @param subject The subject of the shares
+    /// @param shares  The number of shares
+    ///
+    /// @return value The amount of ether that purchasing `shares` shares of `subject` would cost
+    function getBuyPrice(address subject, uint256 shares) external view returns (uint256 value);
+
+    /// @notice Returns the amount of ether that selling `shares` shares of `subject` returns without accounting for fees
+    ///
+    /// @param subject The subject of the shares
+    /// @param shares  The number of shares
+    ///
+    /// @return value The amount of ether that selling `shares` shares of `subject` would return
+    function getSellPrice(address subject, uint256 amount) external view returns (uint256 value);
+
+    /// @notice Returns the amount of ether that purchasing `shares` shares of `subject` costs after accounting for fees
+    ///
+    /// @param subject The subject of the shares
+    /// @param shares  The number of shares
+    ///
+    /// @return value The amount of ether that purchasing `shares` shares of `subject` would cost
+    function getBuyPriceAfterFee(address subject, uint256 shares) external view returns (uint256 value);
+
+    /// @notice Returns the amount of ether that selling `shares` shares of `subject` returns after accounting for fees
+    ///
+    /// @param subject The subject of the shares
+    /// @param shares  The number of shares
+    ///
+    /// @return value The amount of ether that selling `shares` shares of `subject` would return
+    function getSellPriceAfterFee(address subject, uint256 shares) external view returns (uint256 value);
+
+    /// @notice Buys `shares` shares of `subject`
+    ///
+    /// @param subject The subject of the shares
+    /// @param amount  The number of shares to buy
+    function buyShares(address subject, uint256 shares) external payable;
+
+    /// @notice Sells `shares` shares of `subject`
+    ///
+    /// @param subject The subject of the shares
+    /// @param amount  The number of shares to sell
+    function sellShares(address subject, uint256 shares) external payable;
+}
---
+++ src/SemiFungibleFriendShares.sol
@@ -0,0 +1,141 @@
+pragma solidity 0.8.21;
+
+import {Owned} from "solmate/auth/Owned.sol";
+import {ERC1155} from "solmate/tokens/ERC1155.sol";
+import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
+
+import {FriendSharesV1} from "./FriendSharesV1.sol";
+
+contract SemiFungibleFriendShares is ERC1155, Owned {
+    using SafeTransferLib for address;
+
+    /// @notice Emitted when the default URI is set to `value`
+    event DefaultURI(string value);
+
+    /// @notice The `FriendSharesV1` contract that this contract wraps
+    FriendSharesV1 public immutable shares;
+
+    /// @notice A flag indicating if the contract reentrancy lock is unclaimed
+    bool public unlocked = true;
+
+    /// @notice The default URI to return for token ids that have not had their URI set
+    string public defaultURI;
+
+    /// @notice The metadata URI overrides for the token with id `id`
+    mapping(uint256 id => string uri) public override uriOverrides;
+
+    constructor(FriendSharesV1 shares_) Owned(msg.sender) {
+        shares = shares_;
+    }
+
+    /// @dev Engages the reentrancy lock
+    modifier lock() {
+        require(unlocked, "LOCKED");
+        unlocked = false;
+        _;
+        unlocked = true;
+    }
+
+    /// @notice Returns the token id for `subject`
+    function getTokenId(address subject) public pure returns (uint256 id) {
+        id = uint256(uint160(subject));
+    }
+
+    /// @notice Sets the default URI to return for token ids that have not had their URI set
+    function setDefaultURI(string memory value) external onlyOwner {
+        defaultURI = value;
+        emit DefaultURI(value);
+    }
+
+    /// @notice Allows `msg.sender` to set their token URI to `value`
+    function setURI(string memory value) external {
+        uint256 id = getTokenId(msg.sender);
+        require(!uriLocked[id], "URI_LOCKED");
+
+        uriOverrides[id] = value;
+
+        emit URI(value, id);
+    }
+
+    /// @notice Returns the metadata URI of the token with id `id`
+    function uri(uint256 id) public view override returns (string memory) {
+        string memory overrideURI = uriOverrides[id];
+        return bytes(overrideURI).length > 0 ? uriOverrides[id] : defaultURI;
+    }
+
+    /// @notice Returns the amount of ether that purchasing `amount` shares of `subject` would cost
+    ///
+    /// @param subject The subject of the shares
+    /// @param amount  The number of shares to purchase
+    ///
+    /// @return cost The amount of ether that purchasing `amount` shares of `subject` costs
+    function previewPurchase(address subject, uint256 amount) public view returns (uint256 cost) {
+        cost = shares.getBuyPriceAfterFee(subject, amount);
+    }
+
+    /// @notice Returns the amount of ether that selling `amount` shares of `subject` would return
+    ///
+    /// @param subject The subject of the shares
+    /// @param amount  The number of shares to sell
+    ///
+    /// @return proceeds The amount of ether that selling `amount` shares of `subject` returns
+    function previewRedeem(address subject, uint256 amount) public view returns (uint256 proceeds) {
+        proceeds = shares.getSellPriceAfterFee(subject, amount);
+    }
+
+    /// @notice Purchase `amount` shares of `subject` and mint them to `recipient`
+    ///
+    /// @param subject     The subject of the shares
+    /// @param amount      The number of shares to purchase
+    /// @param recipient   The recipient of the shares
+    /// @param maximumCost The maximum amount of ether to spend on the purchase
+    /// @param data        The data to pass to `receiver` through the `ERC1155TokenReceiver` callback function if a contract
+    ///
+    /// @return cost The amount of ether spent on the purchase
+    function purchase(
+        address subject,
+        uint256 amount,
+        address recipient,
+        uint256 maximumCost,
+        bytes memory data
+    ) external payable lock returns (uint256 cost) {
+        require(amount > 0, "INVALID_AMOUNT");
+
+        cost = previewPurchase(subject, amount);
+        require(cost <= maximumCost, "EXCESSIVE_COST");
+
+        shares.buyShares{value: cost}(subject, amount);
+
+        _mint(recipient, getTokenId(subject), amount, data);
+
+        if (address(this).balance > 0) {
+            msg.sender.safeTransferETH(address(this).balance);
+        }
+    }
+
+    /// @notice Redeem `amount` shares of `subject` and transfer the proceeds to `recipient`
+    ///
+    /// @param subject         The subject of the shares
+    /// @param amount          The number of shares to sell
+    /// @param recipient       The recipient of the proceeds
+    /// @param minimumProceeds The minimum amount of ether to receive from the sale
+    ///
+    /// @return proceeds The amount of ether received from the sale
+    function redeem(
+        address subject,
+        uint256 amount,
+        address recipient,
+        uint256 minimumProceeds
+    ) external lock returns (uint256 proceeds) {
+        require(amount >= 0, "INVALID_AMOUNT");
+
+        proceeds = previewRedeem(subject, amount);
+        require(proceeds >= minimumProceeds, "INSUFFICIENT_PROCEEDS");
+
+        shares.sellShares(subject, amount);
+
+        _burn(recipient, getTokenId(subject), amount);
+
+        recipient.safeTransferETH(proceeds);
+    }
+}
